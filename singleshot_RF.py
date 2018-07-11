b0735afd6bd964760e2c26101476f1c355a54a99
htdinani <37074283+htdinani@users.noreply.github.com>
2018-07-11 12:47:21 -0400

    Update singleshot_RF.py

--- singleshot_RF.py
+++ singleshot_RF.py
@@ -3,11 +3,11 @@ import pylab as plt
 from numpy import linalg as LA
 import scipy.sparse as sparse
 def rflt(phi): 
-    th=-0.1 #The threshold for the restarting part of the algorithm
-    cntmax=5 # cntmax is the max of "cnt" in the restarting part of the algorithm. In other words "cntmax" number of measurements is done and if the estimated phase "mu" is not found to be accurate the algorithm restarts by setting "mu" to initial mu and "sigma" to initial sigma 
+    th=-0.10 #The threshold for the restarting part of the algorithm
+    cntmax=3 # cntmax is the max of "cnt" in the restarting part of the algorithm. In other words "cntmax" number of measurements is done and if the estimated phase "mu" is not found to be accurate the algorithm restarts by setting "mu" to initial mu and "sigma" to initial sigma 
     nums=1000 #num of samples drawn from the Gaussian
-    nm=100 #a limit that restricts the number of measurements. However the number of measurements could go beyond 100 because if the estimate is not accurate the counter of the "while" loop initializes. 
-    tau=12.5 #minimum interaction time
+    nm=50 #a limit that restricts the number of measurements. However the number of measurements could go beyond 100 because if the estimate is not accurate the counter of the "while" loop initializes. 
+    tau=12.5#minimum interaction time
     T2=1300.0 #decoherence time
     muin=0.0 # mean of the initial Gaussian
     mu=muin
@@ -23,7 +23,7 @@ def rflt(phi):
     muf=0.0
     sigmaf=0.0
     muv=[] #an array to keep track of the means
-    sigmav=[] #an array to keep trakc of the variances
+    sigmav=[] #an array to keep track of the variances
     sigmav.append(sigmain)
     muv.append(mu)
     cnt=0
@@ -34,17 +34,19 @@ def rflt(phi):
         sigma10=0.0
         mu0=0.0
         tott=tott+t #This calculates total interaction time
-        tr = t/T2
-        vis=np.exp(-tr**2)
+        #tr = t/T2
+        vis=np.exp(-(t/T2)**2)
         xr=np.random.uniform(0,1)
-        p0=0.5*(1.0+vis*np.cos(t*phi/tau-theta))
+        #p0=0.5*(1.0+vis*np.cos((t*phi-theta)/tau))
+        p0=0.5*(1.0+vis*np.cos(t*(phi/tau)-theta))
         if xr<=p0: #u1 is the measurement result
             u1=0.0 
         else:
             u1=1.0
         n3=0
         phis=np.random.normal(mu,np.sqrt(sigma),nums) #samples drawn from the Gaussian with mean "mu" and variance "sigma"
-        pps=0.5*(1.0+(-1.0)**u1*vis*np.cos(t*phis/tau-theta)) # The probability that the drawn samples "phis" gives the measurement result "u1"
+        #pps=0.5*(1.0+(-1.0)**u1*vis*np.cos(t*phis/tau-theta/tau)) # The probability that the drawn samples "phis" gives the measurement result "u1"
+        pps=0.5*(1.0+(-1.0)**u1*vis*np.cos(t*(phis/tau)-theta))
         maxpps=np.amax(pps)
         kappae=np.random.uniform(maxpps,1) #this scales the above probability "pps" and the samples are accepted based on pps/kappae. It is a random number between max of p(phis|u1) and 1 (see PRL 117, 010503 (2016)). Setting "kappae" to 1 does not significantly change the final result.
         for n2 in range(1,nums):
@@ -83,19 +85,19 @@ def rflt(phi):
                 cnt=cnt+1
             else: #In this case the estimate is not accurate and the variance "sigma" and the mean "mu" needs to be reset. Here we set the variance to the initial variance "sigmain".
                 cnt=0
-                sigma=sigmain#100*np.amin(sigmav)
-                mu=muin#muv[np.argmin(sigmav)]
+                sigma=sigmav[n1-1]#100*np.amin(sigmav)
+                mu=muv[n1-1]#[np.argmin(sigmav)]
                 #n1=1
         else:
             cnt=cnt+1
         #########################################################
-        t1=np.ceil(1.25/np.sqrt(sigma))*tau #This calculates the interaction time for the next measurement based on the current variance "sigma".
-        t=np.minimum(t1,T2) #If the interaction time is bigger than the decoherence time we choose t=T2
-        theta=np.random.normal(mu,np.sqrt(sigma)) #The controlled phase for the next measurement
+        t1=np.ceil(1.25/np.sqrt(sigma)) #This calculates the interaction time for the next measurement based on the current variance "sigma".
+        t=np.minimum(t1*tau,T2) #If the interaction time is bigger than the decoherence time we choose t=T2
+        theta=np.random.normal(mu,np.sqrt(sigma))*t1 #The controlled phase for the next measurement
         n1=n1+1
-    sigmaf=sigma#np.amin(sigmav) 
-    #sfl=np.argmin(sigmav)#minloc[sigmav]
-    muf=mu#muv[sfl]
+    sigmaf=np.amin(sigmav) 
+    sfl=np.argmin(sigmav)#minloc[sigmav]
+    muf=muv[sfl]
     #op=(muf, sigmaf, sfl, tott)
     op=(muf, sigmaf, tott)
     return op
@@ -105,7 +107,7 @@ dimtot=1000
 sphiv=[]
 sharp=[]
 timev=[]
-#np.random.seed(305)
+np.random.seed(12345)
 fName = r'C:\HTD\data\singleshotrf.txt'
 tf = open(fName, 'w+')
 for cntr in range(0,dimtot):
@@ -114,7 +116,7 @@ for cntr in range(0,dimtot):
     #print(sphi)
     sphiv.append(sphi)
     musig=rflt(sphi)
-    #print('mu and sigma')
+    #print('mu')
     #print(musig[0])
     #print(musig[1])
     #int(input('input an integer'))
